30ce5e0c2964318c6ebc4dd98f0c5d9c6ee8c740
CutiePizza <ines.chokri97@gmail.com>
2020-07-13 15:14:36 +0100

    Fixed the build

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # This script  initialize a C# project and build it
-dotnet new console -o 0-new_project
-dotnet build -o 0-new_project
+dotnet new console -o 1-new_project
+cd 1-new_project/
+dotnet build
