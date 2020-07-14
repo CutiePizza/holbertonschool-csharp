d06b7ea6cd9fc1546cf36d3fc43aefb02a2a5d54
CutiePizza <ines.chokri97@gmail.com>
2020-07-14 17:27:28 +0100

    print diaginale

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    class Line
+    {
+        public static void PrintDiagonal(int length)
+        {
+            int i, j;
+
+            if (length <= 0)
+		        Console.WriteLine();
+            else
+            {
+                for (i = 0; i < length; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("\\");
+                    }
+                    else
+                    {
+                        for (j = 0; j < i; j++)
+                        {
+                           Console.Write(' ');
+                        }
+                        Console.WriteLine("\\");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
