c335a07d33dffcb9b7bcc6ad7a6d88a0eac695ea
CutiePizza <ines.chokri97@gmail.com>
2020-07-14 16:28:36 +0100

    print combination of two digit

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i < 10; i++)
+                {
+                    for (j = i + 1; j < 10; j++)
+                    {
+                        Console.Write(i);
+                        Console.Write(j);
+                        if (i < 8 || j < 9)
+                        {
+                            Console.Write(", ");
+                        }
+
+                    }
+                }
+                Console.WriteLine();
+        }
+    }
+}
