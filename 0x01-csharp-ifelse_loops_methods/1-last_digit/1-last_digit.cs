89f3b3c39848762215e6327b8c164f37a6a8f38a
CutiePizza <ines.chokri97@gmail.com>
2020-07-14 13:49:13 +0100

    fix

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,15 +8,15 @@ class Program
         int number = rndm.Next(-10000, 10000);
         if (number % 10 > 5)
         {
-            Console.WriteLine("Last digit of " + number + " is " + number % 10 + " and is greater than 5");
+            Console.WriteLine("The last digit of " + number + " is " + number % 10 + " and is greater than 5");
         }
         else if (number % 10 == 0)
         {
-            Console.WriteLine("Last digit of " + number + " is " + number % 10 + " and is 0");
+            Console.WriteLine("The last digit of " + number + " is " + number % 10 + " and is 0");
         }
         else if (number % 10 < 6)
         {
-            Console.WriteLine("Last digit of " + number + " is " + number % 10 + " and is less than 6 and not 0");
+            Console.WriteLine("The last digit of " + number + " is " + number % 10 + " and is less than 6 and not 0");
         }
     }
 }
