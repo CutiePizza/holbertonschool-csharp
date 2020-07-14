a125aa84593d3fb51f793a7c4b9df688dad2e117
CutiePizza <ines.chokri97@gmail.com>
2020-07-14 16:06:17 +0100

    print numbers

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            char j = '0';
+            string k = ", ";
+            for (i = 0; i < 100 ; i++)
+            {
+                if (i >= 10)
+                {
+                        j = ' ';
+                        k = ",";
+                }
+                if (i != 99)
+                {
+                    Console.Write("{0}{1}{2}", j, i, k);
+                }
+                else
+                    Console.Write(i);
+            }
+        }
+    }
+}
