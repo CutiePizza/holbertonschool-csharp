8e5b4ef525e1910c575cddd53db15259d570ecf1
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 17:02:56 +0100

    create and print list of int

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+            List<int> lista = new List<int>();
+            if (size != 0)
+            {
+            for (int i = 0; i < size; i++)
+            {
+                lista.Add(i);
+                if (i != size -1)
+                    Console.Write($"{lista[i]} ");
+                else
+                    Console.WriteLine($"{lista[i]}");
+            }
+            }
+            else
+                Console.WriteLine();
+            return(lista);
+        }
+    }
