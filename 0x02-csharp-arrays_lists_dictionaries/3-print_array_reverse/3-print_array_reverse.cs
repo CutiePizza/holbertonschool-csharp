96fa27f0f9b2a4a965c80b10ea8fefec83dffec0
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 16:51:30 +0100

    print array in reverse

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    class Array
+    {
+        public static void Reverse(int[] array)
+        { 
+         if (array != null && array.Length > 0)
+         {
+             for(int i = array.Length-1; i >= 0; i--)
+                {
+                    if(i != 0)
+                        Console.Write($"{array[i]} ");
+                    else
+                        Console.Write($"{array[i]}\n");
+                }
+         }
+         else
+            Console.WriteLine();
+        }
+    }
