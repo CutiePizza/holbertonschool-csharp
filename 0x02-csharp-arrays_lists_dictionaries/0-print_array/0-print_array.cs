4aa7358957b0a7e07308428492ad9dd7e154d80d
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 16:09:17 +0100

    create and print array of int

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,14 +9,18 @@
                 Console.WriteLine("Size cannot be negative");
                 return (null);
             }
-
             int[] array = new int[size];
+            
+            if (size > 0)
+            {
             int i;
-
-            for (i = 0; i < array.Length; i++)
+            for (i = 0; i < array.Length - 1; i++)
             {
                 array[i] = i;
-                Console.Write(array[i] + " ");
+                Console.Write($"{array[i]} ");
+            }
+            array[i] = i;
+            Console.Write(array[i]);
             }
             Console.WriteLine();
             return (array);
