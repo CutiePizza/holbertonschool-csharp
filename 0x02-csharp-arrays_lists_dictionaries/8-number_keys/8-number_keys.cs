700f57b53befe154a1a4ea36f8f750803e5375d2
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 17:42:25 +0100

    returns number of keys of a dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+    class Dictionary
+    {
+        public static int NumberOfKeys(Dictionary<string, string> myDict)
+        {
+            int num = 0;
+            foreach (var item in myDict)
+                num++;
+            return(num);
+        }
+    }
