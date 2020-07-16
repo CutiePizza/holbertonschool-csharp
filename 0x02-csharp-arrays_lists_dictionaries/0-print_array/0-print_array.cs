f6712b1ebbfec18b5a0629e5d3323f868521e992
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 15:58:58 +0100

    create and print array of int

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class Array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+
+            int[] array = new int[size];
+            int i;
+
+            for (i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            return (array);
+        }    
+    }
