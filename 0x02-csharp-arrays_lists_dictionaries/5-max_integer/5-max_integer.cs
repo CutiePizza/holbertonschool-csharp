6422d74dd6bdf1a9cc761aa956877de409c24228
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 17:11:47 +0100

    return largest num in list

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+        {
+            if (myList.Count > 0)
+            {
+                myList.Sort();
+                return (myList[myList.Count - 1]);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+                return (-1);
+            }
+        }
+    }
