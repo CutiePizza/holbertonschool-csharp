de66bc20b07363a26801e34be6225e0265c7a4fb
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 20:40:47 +0100

    best score

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+    class Dictionary
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            int score = 0;
+            string ok = string.Empty;
+            if (myList.Count > 0)
+            {
+            foreach (KeyValuePair<string, int> entry in myList)
+            {   
+                if (entry.Value > score)
+                {
+                    score = entry.Value;
+                    ok = entry.Key;
+                }
+            }
+            return (ok);
+            }
+            return ("None");
+        }
+    }
