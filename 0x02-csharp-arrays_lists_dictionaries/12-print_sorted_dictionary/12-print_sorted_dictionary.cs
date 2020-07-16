f04d8aa8d4e4e228f11157812afae3ebc08f06c9
CutiePizza <ines.chokri97@gmail.com>
2020-07-16 19:53:35 +0100

    print dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            List<string> lista = new List<string>();
+            foreach(var ok in myDict)
+            {   
+                lista.Add(ok.Key);
+            }
+            lista.Sort();
+            foreach(string name in lista)
+            {
+                Console.WriteLine($"{name}: {myDict[name]}");
+            }
+        }
+    }
