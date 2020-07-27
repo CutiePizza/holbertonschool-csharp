2881dfa9749f1b8be573cd0065b36b6ea959d6bc
CutiePizza <ines.chokri97@gmail.com>
2020-07-27 14:29:22 +0100

    task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static List<int> CommonElements(List<int> list1, List<int> list2)
+        {
+            List<int> lista = new List<int>();
+            foreach (int number1 in list1)
+            {
+               foreach (int number2 in list2)
+               {
+                   if (number1 == number2)
+                   {
+                       lista.Add(number1);
+                   }
+               }
+            }
+            lista.Sort();
+            return (lista);
+        }
+    }
