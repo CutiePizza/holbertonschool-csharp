0263ae3d2c8221b463d6abc323edb64402e784f2
CutiePizza <ines.chokri97@gmail.com>
2020-07-27 13:48:51 +0100

    trying Linq methods

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            List<int> lista = myList.Distinct().ToList();
+            int sum = lista.Sum();
+            return (sum);
+        }
+    }
