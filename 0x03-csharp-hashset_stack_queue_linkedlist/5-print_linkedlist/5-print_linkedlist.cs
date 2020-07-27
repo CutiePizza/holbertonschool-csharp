4fed09b2b5b91d9220de32ed668ee35539ee598a
CutiePizza <ines.chokri97@gmail.com>
2020-07-27 18:00:02 +0100

    task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+    class LList
+    {
+        public static LinkedList<int> CreatePrint(int size)
+        {
+            LinkedList<int> llista = new LinkedList<int>();
+            if (size > 0)
+            {
+                for (int i = 0; i < size; i++)
+                    llista.AddLast(i);
+                foreach (int num in llista)
+                    Console.WriteLine(num);
+            }
+            return (llista);
+        }
+    }
