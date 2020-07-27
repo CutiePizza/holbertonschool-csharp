4fc0d84c5c403b47a0f919b1813395b5a0b55287
CutiePizza <ines.chokri97@gmail.com>
2020-07-27 21:49:15 +0100

    task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+    class LList
+    {
+        public static void Delete(LinkedList<int> myLList, int index)
+        {
+            LinkedListNode<int> node = myLList.First;
+            int i = 0;
+            while(myLList.Count > i && node.Next != null)
+            {
+                if (i == index)
+                {
+                    myLList.Remove(node);
+                }
+                node = node.Next;
+                i++;
+            }
+
+        }
+    }
