34ddb2603a7bc0c1322cc3eccfc4f8540945dace
CutiePizza <ines.chokri97@gmail.com>
2020-07-28 20:04:48 +0100

    task 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            int res = 0;
+            try
+            {
+                res = a / b;
+            }
+            catch (DivideByZeroException) {
+                
+        }
+        finally {
+            Console.WriteLine($"{a} / {b} = {res}");
+        }
+    }
+}
