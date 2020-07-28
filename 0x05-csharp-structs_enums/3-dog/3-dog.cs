6ba9b4e9cb40c9b86f7776486faf95716f1e0569
CutiePizza <ines.chokri97@gmail.com>
2020-07-28 21:48:55 +0100

    tsk 3

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,28 @@
+﻿public enum Rating {
+    Good,
+    Great,
+    Excellent
+}
+public struct Dog {
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+    public override string ToString ()
+    {
+        return base.ToString() + " Name: " + this.name.ToString()
+        + "\nAge: " + this.age.ToString()
+        + "\nOwner: " + this.owner.ToString()
+        + "\nRating: " + this.rating.ToString();
+    }
+}
+
+
