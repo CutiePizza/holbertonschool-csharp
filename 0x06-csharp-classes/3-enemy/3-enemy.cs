37dcef4f871f9fd597f64a3efa64546bd8fee270
CutiePizza <ines.chokri97@gmail.com>
2020-07-30 23:00:31 +0200

    task 3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a class of Zombie
+    ///</summary>
+    public class Zombie
+    {
+       // Health of a zombie
+        private int health;
+        /// <summary> Initializes a new instance of a Zombie </summary> ///
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary> Initializes a new instance of a Zombie with a given health value</summary> ///
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            else
+                this.health = value;
+        }
+        /// <summary>
+        /// Gets the health of a zombie
+        /// </summary>
+        /// <returns>an integer</returns>
+        public int GetHealth()
+        {
+            return (this.health);
+        }
+    }
+}
