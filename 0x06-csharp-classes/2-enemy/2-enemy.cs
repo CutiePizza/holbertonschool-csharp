7768f6f725cd4e6338f4c5eba2f58cb7032f6d0c
CutiePizza <ines.chokri97@gmail.com>
2020-07-30 22:50:31 +0200

    task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a class of Zombie
+    ///</summary>
+    public class Zombie
+    {
+        /// <summary> 
+        /// Health of a zombie
+        /// </summary>
+        public int health;
+        /// <summary> Initializes a new instance of a Zombie </summary> ///
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary> Initializes a new instance of a Zombie with a given health value</summary> ///
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            else
+                this.health = value;
+        }
+    }
+}
