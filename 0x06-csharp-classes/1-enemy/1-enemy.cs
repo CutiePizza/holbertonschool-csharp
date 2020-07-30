85135d15595ecc9ad88eca14405ee64de874e6f0
CutiePizza <ines.chokri97@gmail.com>
2020-07-30 22:43:02 +0200

    task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a class of Zombie
+    ///</summary>
+    public class Zombie
+    {
+        /// <summary> 
+        /// Health of a zombie
+        /// </summary>
+        public int health;
+        /// <summary> Initializes a new instance of a Zombie </summary> ///
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
