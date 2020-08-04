5656a2bcf122e6248299fd57d8c35ecbeb6fae55
CutiePizza <ines.chokri97@gmail.com>
2020-08-04 20:21:08 -0300

    tests added

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -6,6 +6,10 @@ namespace MyMath.Tests
     {
         List<int> lista = new List<int>() {10, 15, 0, -3, 288, 100, 3};
         List<int> lista2 = new List<int>() {};
+
+        List<int> lista3 = new List<int>() {-5, -10, -8, -7, -14};
+
+        List<int> lista4 = new List<int>() {2};
         [SetUp]
         public void Setup()
         {
@@ -23,6 +27,20 @@ namespace MyMath.Tests
         {
             int n = Operations.Max(lista2);
             Assert.AreEqual(0, n);
+        }
+
+        [Test]
+        public void OnlyNegatives()
+        {
+            int n = Operations.Max(lista3);
+            Assert.AreEqual(-5, n);
+        }
+
+        [Test]
+        public void OneElement()
+        {
+            int n = Operations.Max(lista4);
+            Assert.AreEqual(2, n);
         }
     }
 }
