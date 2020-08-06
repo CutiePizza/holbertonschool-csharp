bda1c847d6d725a115d2d770dcc4f796d32adc69
CutiePizza <ines.chokri97@gmail.com>
2020-08-06 21:33:59 +0100

    task 6

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+    class Shape
+    {
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private int width;
+        private int height;
+        public int Width {
+            get => this.width;
+            set { 
+                if (value < 0)
+                    throw new ArgumentException("Width must be greater than or equal to 0");
+                else
+                    this.width = value;
+            }
+        }
+        public int Height {
+            get => this.height;
+            set { 
+                if (value < 0)
+                    throw new ArgumentException("Height must be greater than or equal to 0");
+                else
+                    this.height = value;
+            }
+        }
+    }
+
