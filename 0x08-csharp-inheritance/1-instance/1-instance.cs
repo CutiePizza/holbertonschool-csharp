c1d56d05a739cb6a7d7d39c18c7eb2773ec01cae
CutiePizza <ines.chokri97@gmail.com>
2020-08-06 19:12:18 +0100

    task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Class that represents an object
+/// </summary>
+    class Obj
+    {
+        /// <summary>
+        /// Checks if the obj is an instance of Array
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true or false</returns>
+        public static bool IsInstanceOfArray(object obj)
+        {
+            if (obj is Array || obj.GetType() == typeof(Array))
+                return (true);
+            else
+                return (false);
+        }
+    }
