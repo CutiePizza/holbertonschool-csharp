a16537f73f0127a323e6a7704f9cb2e1b838973e
CutiePizza <ines.chokri97@gmail.com>
2020-08-06 20:26:45 +0100

    task 4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/// <summary>
+/// Class represents an animal
+/// </summary>
+public class Animal
+{
+
+}
+/// <summary>
+/// Class represents a dog
+/// </summary>
+public class Dog : Animal
+{
+        
+}
