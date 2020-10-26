1a0fb389e012f06e3004f75c987f38a68fd63f7d
CutiePizza <ines.chokri97@gmail.com>
2020-10-26 20:57:28 -0300

    task 0 completed

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
-namespace _0_queue
+/// <summary>
+/// QUeue Class
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
 {
-    class Program
+    /// <summary>
+    /// Check Type of a queue
+    /// </summary>
+    /// <returns>Type of a Queue</returns>
+    public Type CheckType()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        return (this.GetType());
     }
 }
