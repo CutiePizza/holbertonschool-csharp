5a9d4ede882d562c70db4ae9a303c6d906de5a38
CutiePizza <ines.chokri97@gmail.com>
2020-10-26 23:53:38 -0300

    task 5 not completed

---
+++ 0x0A-csharp-generics/5-concatenate/5-main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> myStrQ = new Queue<string>();
+
+        myStrQ.Enqueue("hello");
+        myStrQ.Enqueue("holberton");
+        myStrQ.Enqueue("school");
+
+        Console.WriteLine(myStrQ.Concatenate());
+
+        Console.WriteLine("----------");
+
+        Queue<int> myIntQ = new Queue<int>();
+        myIntQ.Concatenate();
+
+        Console.WriteLine("----------");
+
+        Queue<int> myCharQ = new Queue<int>();
+        myCharQ.Enqueue(1);
+        myCharQ.Enqueue(2);
+        myCharQ.Enqueue(3);
+        Console.WriteLine(myCharQ.Concatenate());
+    }
+}
