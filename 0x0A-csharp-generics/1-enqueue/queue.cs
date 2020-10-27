43f6032ec4ce8b50b84a69588bbce61197812d15
CutiePizza <ines.chokri97@gmail.com>
2020-10-26 22:01:13 -0300

    task 1

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,88 @@
+﻿using System;
+/// <summary>
+/// QUeue Class
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+
+    Node head = null;
+    Node tail = null;
+    int count;
+
+    
+    /// <summary>
+    /// Check Type of a queue
+    /// </summary>
+    /// <returns>Type of a Queue</returns>
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+
+    /// <summary>
+    /// Enqueue
+    /// </summary>
+    /// <param name="value">value to put</param>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (this.tail == null)
+        {
+            this.head = this.tail = newNode;
+        }
+        else
+        {
+            this.tail.Next = newNode;
+            this.tail = newNode;
+        }
+
+    }
+
+    public int Count()
+    {
+        Node p = this.head;
+        int i = 0;
+        while (p != null)
+        {
+            p = p.Next;
+            i++;
+        }
+        return i;
+    }
+
+
+    /// <summary>
+    /// Class Node
+    /// </summary>
+    public class Node
+    {
+        T value;
+        Node next = null;
+
+        public T Value {
+            get => this.value;
+            set {
+                this.value = value;
+            }
+        }
+
+        public Node Next {
+            get => this.next;
+            set {
+                this.next = value;
+            }
+        }
+        /// <summary>
+        /// Node constructor
+        /// </summary>
+        /// <param name="value">Value</param>
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
+
+
