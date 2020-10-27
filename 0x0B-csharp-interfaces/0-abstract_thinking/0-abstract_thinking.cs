ead4facf200381a57c0e0246ffe5c8aeb9ab5f63
CutiePizza <ines.chokri97@gmail.com>
2020-10-27 19:56:15 -0300

    task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+/// <summary>
+/// Base class
+/// </summary>
+class Base
+{
+    /// <summary>
+    /// Property name
+    /// </summary>
+    /// <value>String value</value>
+    public string name {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Override tostring() method
+    /// </summary>
+    /// <returns>string</returns>
+
+    public override string ToString()
+    {
+        return ($"{this.name} is a {this.GetType()}");
+    }
+}
