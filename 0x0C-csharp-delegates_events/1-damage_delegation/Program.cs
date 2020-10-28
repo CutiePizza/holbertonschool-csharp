c925ab6ef5857e0b63caa227d2364f171c5e95ac
CutiePizza <ines.chokri97@gmail.com>
2020-10-28 18:51:43 -0300

    task1

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+/// <summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    /// <summary>
+    /// Name of the player
+    /// </summary>
+    /// <value></value>
+    private string name {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Max Health Points of player
+    /// </summary>
+    /// <value></value>
+    private float maxHp {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Actual health
+    /// </summary>
+    /// <value></value>
+    private float hp {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp > 0f)
+            this.maxHp = maxHp;
+        else
+        { 
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+
+    }
+
+    /// <summary>
+    /// Print Player's health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+
+    /// <summary>
+    /// TakeDamage method
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(float damage)
+    {
+        if (damage > 0)
+        {
+            Console.WriteLine($"{this.name} takes {damage} damage!");
+            this.hp -= damage;
+        }
+        else
+            Console.WriteLine($"{this.name} takes 0 damage!");
+    }
+
+    /// <summary>
+    /// HealDamage method
+    /// </summary>
+    /// <param name="heal"></param>
+    public void HealDamage(float heal)
+    {
+        if (heal > 0)
+        {
+            Console.WriteLine($"{this.name} heals {heal} HP!");
+            this.hp += heal;
+        }
+        else
+            Console.WriteLine($"{this.name} takes 0 heals!");
+    }
+
+}
+/// <summary>
+/// Delegate
+/// </summary>
+/// <param name="amount"></param>
+public delegate void CalculateHealth(float amount);
+
