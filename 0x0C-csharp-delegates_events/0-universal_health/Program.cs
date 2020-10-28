2c4ee35295bafb3d783d9a78dff62dc628b58cd1
CutiePizza <ines.chokri97@gmail.com>
2020-10-28 18:14:06 -0300

    Class Player

---
+++ 0x0C-csharp-delegates_events/0-universal_health/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+/// <summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    /// <summary>
+    /// Name of the player
+    /// </summary>
+    /// <value></value>
+    public string name {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Max Health Points of player
+    /// </summary>
+    /// <value></value>
+    public float maxHp {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Actual health
+    /// </summary>
+    /// <value></value>
+    public float hp {
+        get;
+        set;
+    }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp > 0f)
+            this.maxHp = maxHp;
+        else
+        { 
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+
+    }
+
+    /// <summary>
+    /// Print Player's health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
+
